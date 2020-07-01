483332c4118668d085ad589974f4cef39a188d8a
sharapov2701 <50914537+sharapov2701@users.noreply.github.com>
2020-07-01 22:45:50 +0300

    init

---
+++ public/app.js
@@ -0,0 +1,41 @@
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru-RU', {
+        currency: 'rub',
+        style: 'currency'
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = toCurrency(node.textContent)
+})
+
+const $cart = document.querySelector('#cart')
+
+if ($cart) {
+    $cart.addEventListener('click', event => {
+        if (event.target.classList.contains('js-remove')) {
+            const id = event.target.dataset.id
+            fetch('/cart/remove/' + id, { method: 'delete' })
+            .then(d => d.json())
+            .then(cart => {
+                if (cart.courses.length) {
+                    const html = cart.courses.map(c => {
+                        return `
+                            <tr>
+                                <td>${c.title}</td>
+                                <td>${c.count}</td>
+                                <td>
+                                    <button class="btn btn-small js-remove" data-id="${c.id}">Удалить</button>
+                                </td>
+                            </tr>
+                        `
+                    }).join('')
+                    $cart.querySelector('tbody').innerHTML = html
+                    $cart.querySelector('.price').textContent = toCurrency(cart.price)
+                } else {
+                    $cart.innerHTML = '<p>Корзина пуста</p>'
+                }
+            })
+        }
+    })
+}
