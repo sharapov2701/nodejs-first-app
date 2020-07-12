2807b80146c4e729b8176d416aa87cfe3a5cf0c1
sharapov2701 <50914537+sharapov2701@users.noreply.github.com>
2020-07-12 23:25:41 +0300

    deploy

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://admin:LhIMMwT5n9yr8dhh@cluster0.756jk.mongodb.net/shop',
-    SESSION_SECRET: 'some secret',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'sharapov2701@gmail.com',
-    BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
